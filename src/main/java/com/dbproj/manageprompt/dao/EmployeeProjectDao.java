5dfcb16b0c60eff3403632f39c45ba91ee264338
HyunSBong <sanseng@mju.ac.kr>
2023-05-20 17:57:40 +0900

    동료/PM 평가 등록

--- src/main/java/com/dbproj/manageprompt/dao/EmployeeProjectDao.java
+++ src/main/java/com/dbproj/manageprompt/dao/EmployeeProjectDao.java
@@ -6,4 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface EmployeeProjectDao extends JpaRepository<EmployeeProjectEntity, Long> {
 
     EmployeeProjectEntity findByProjectEntity_ProIdAndAndEmployeeEntity_EmpId(String proId, Long empId);
+
+    EmployeeProjectEntity findByProjectEntity_ProNameAndAndEmployeeEntity_EmpId(String proName, Long empId);
 }
