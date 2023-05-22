2afd95554e83b37209a987fb12340a082500e1dc
HyunSBong <sanseng@mju.ac.kr>
2023-05-22 14:21:16 +0900

    버그 수정

--- src/main/java/com/dbproj/manageprompt/dto/ClientEvaluationResponseDto.java
+++ src/main/java/com/dbproj/manageprompt/dto/ClientEvaluationResponseDto.java
@@ -1,24 +1,24 @@
-//package com.dbproj.manageprompt.dto;
-//
-//import com.dbproj.manageprompt.entity.ClientInfoEntity;
-//
-//import lombok.*;
-//
-//import java.util.List;
-//import java.util.stream.Collectors;
-//
-//@Data
-//@Builder
-//@AllArgsConstructor
-//@NoArgsConstructor(access = AccessLevel.PRIVATE)
-//@Getter
-//public class ClientEvaluationResponseDto {
-//    private Long clientId;
-//
-//    private String clientName;
-//
-//    private List<EmployeeProjectEvaluationResponseDto> projectEvaluationList;
-//
+package com.dbproj.manageprompt.dto;
+
+import com.dbproj.manageprompt.entity.ClientInfoEntity;
+
+import lombok.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+@Getter
+public class ClientEvaluationResponseDto {
+    private Long clientId;
+
+    private String clientName;
+
+    private List<EmployeeProjectEvaluationResponseDto> projectEvaluationList;
+
 //    public static ClientEvaluationResponseDto from(ClientInfoEntity clientInfoEntity) {
 //        return ClientEvaluationResponseDto.builder()
 //                .clientId(clientInfoEntity.getClientId())
@@ -27,4 +27,4 @@
 //                .projectEvaluationList(clientInfoEntity.getEmployeeProjectEntities().stream().map(EmployeeProjectEvaluationResponseDto::from).collect(Collectors.toList()))
 //                .build();
 //    }
-//}
+}
